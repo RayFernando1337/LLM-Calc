b71adfaa077bf1210a753771679fdf157612e713
Ray Fernando <104695450+RayFernando1337@users.noreply.github.com>
2024-04-27 21:19:50 +0000

    Added subscribe iframe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+
 def calculate_max_parameters(available_ram_gb, bits_per_parameter, os_overhead_gb=2):
     """
     Calculate the maximum number of parameters that can fit in the available RAM.
@@ -38,6 +39,7 @@ def main():
     """
     st.title('LLM RAM Calculator')
 
+
     st.sidebar.title('Settings')
     available_ram = st.sidebar.number_input('Enter your available RAM in GB:', min_value=1.0, value=16.0, step=8.0)
     os_overhead_gb = st.sidebar.slider('Estimated OS RAM Usage (GB):', min_value=1.0, max_value=8.0, value=2.0, step=0.5,
@@ -62,6 +64,10 @@ def main():
     st.sidebar.write('- **8-bit**: Standard in many LLM deployments. Good balance of size, speed, and accuracy.')
     st.sidebar.write('- **fp16, bfloat16**: Half-precision formats offering significant reduction in size with less impact on accuracy than integer quantization. Widely supported on modern GPUs.')
     st.sidebar.write('- **fp32**: Full precision used during model training, rarely used in deployment due to high computational costs.')
+
+    st.markdown("""
+        <iframe src="https://embeds.beehiiv.com/b159f8b5-03b7-41b3-b454-877016993ea1" data-test-id="beehiiv-embed" width="100%" height="320" frameborder="0" scrolling="no" style="border-radius: 4px; border: 2px solid #e5e7eb; margin: 0; background-color: transparent;"></iframe>
+    """, unsafe_allow_html=True)
 
 if __name__ == '__main__':
     main()
